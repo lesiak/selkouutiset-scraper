fd95116d4642a6762ecfc35a31574365d24750ad
Adam Lesiak <lesiak@outlook.com>
2023-06-21 22:52:35 +0200

    Copy text to clipboard

--- scrape.js
+++ scrape.js
@@ -1,3 +1,28 @@
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+
 const content = document.querySelector(".yle__article__content")
 const contentElems = Array.from(content.querySelectorAll("h2.yle__article__heading, p.yle__article__paragraph"))
 
@@ -18,3 +43,4 @@ quizElems.forEach(c => {
 });
 
 console.log(s);
+fallbackCopyTextToClipboard(s);
